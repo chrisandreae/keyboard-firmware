cf00a887b155d15f06dcfec05ffc0b6e4a5efd22
Chris Andreae <chris@andreae.gen.nz>
2014-02-16 23:18:26 +0900

    Remove old Ruby/GTK GUI

--- client/KeyboardClient.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/ruby
-
-require_relative "keyboardcomm"
-require_relative "hidcodes"
-require_relative "keyboardmodel"
-require_relative "keyboardpresenter"
-require_relative "keyboardview"
-
-KeyboardPresenter.new.showAction
