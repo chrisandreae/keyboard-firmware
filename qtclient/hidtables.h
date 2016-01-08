6333d8e02876a158c6727ed43f4b69a8f9fee553
Andrew Childs <lorne@cons.org.nz>
2016-01-08 23:59:47 +1300

    qtclient: Force keypad and program to be the same on all layers

--- qtclient/hidtables.h
+++ qtclient/hidtables.h
@@ -10,6 +10,11 @@ public:
 	enum HIDModelRoles {
 		UsageCode = Qt::UserRole + 1
 	};
+	enum HIDUsages {
+		HIDUsageProgram     = 0xfe,
+		HIDUsageKeypadShift = 0xfc,
+		HIDUsageNoKey       = 0xff,
+	};
 	static const char *nameUsage(uint8_t usage);
 	static QStandardItemModel *newUsageModel(QObject *parent = NULL);
 };
--- qtclient/layoutpresenter.cc
+++ qtclient/layoutpresenter.cc
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QDebug>
 
+#include "hidtables.h"
 #include "layoutpresenter.h"
 #include "layout.h"
 #include "keyboardmodel.h"
@@ -24,9 +25,52 @@ void LayoutPresenter::setModel(const QSharedPointer<KeyboardModel>& model) {
 	mView->setMapping(*mModel->getMapping());
 }
 
+
+/* Call given function with each logical keycode corresponding to the
+ * physical keycode in every layer. */
+template <class UnaryFunction>
+static void forEachLogicalKey(const Layout& layout, PhysicalKeycode pkey, UnaryFunction f) {
+	unsigned int layer = 0;
+	LogicalKeycode logicalKey;
+	while ((logicalKey = layout.physicalKeycodeToLogical(pkey, layer++)) <
+	       layout.mappingSize())
+	{
+		f(logicalKey);
+	}
+}
+
+// special keys like keypad and program must be the same on all
+// layers.
+static bool keyIsSpecial(HIDKeycode hidKey) {
+	return (hidKey == HIDTables::HIDUsageProgram ||
+	        hidKey == HIDTables::HIDUsageKeypadShift);
+}
+
 void LayoutPresenter::setHIDUsage(LogicalKeycode logicalKey, HIDKeycode hidKey) {
 	QByteArray *mapping = mModel->getMapping();
-	(*mapping)[logicalKey] = hidKey;
+
+	const auto& layout = *mModel->getLayout();
+
+	PhysicalKeycode pkey =
+		layout.logicalKeycodeToPhysical(logicalKey);
+
+	// new key is special => set on all layers
+	if (keyIsSpecial(hidKey)) {
+		forEachLogicalKey(layout, pkey, [&](LogicalKeycode logicalKey){
+			(*mapping)[logicalKey] = hidKey;
+		});
+	}
+	else {
+		// old key was special => clear other layers
+		if (keyIsSpecial((*mapping)[logicalKey])) {
+			forEachLogicalKey(layout, pkey, [&](LogicalKeycode logicalKey){
+				(*mapping)[logicalKey] = HIDTables::HIDUsageNoKey;
+			});
+		}
+
+		(*mapping)[logicalKey] = hidKey;
+	}
+
 	// and update the view
 	mView->setMapping(*mapping);
 }
