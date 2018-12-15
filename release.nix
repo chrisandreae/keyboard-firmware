34ecdfc54e443e0db4fcb8636d7384ac425333ec
Andrew Childs <lorne@cons.org.nz>
2018-12-15 20:41:16 +0900

    Nix: use default ghc version

--- release.nix
+++ release.nix
@@ -11,7 +11,7 @@ rec {
     inherit compiler;
   };
 
-  compiler = haskell.packages.ghc802.callPackage ./nix/compiler.nix {};
+  compiler = haskellPackages.callPackage ./nix/compiler.nix {};
 
   kinesis = mkFirmware {
     name = "kinesis";
